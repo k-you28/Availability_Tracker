cd1a6968d09878a493fd667488fdf8f14017babf
k-you28 <kevinyou@Kevins-MacBook-Air.local>
2023-06-29 10:50:37 -0500

    Added JSON algo to CampResortsByMonth.java

--- CampResortsByMonth.java
+++ CampResortsByMonth.java
@@ -8,6 +8,10 @@ import java.net.URL;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
 import java.io.PrintWriter;
 
 public class CampResortsByMonth {
@@ -19,9 +23,10 @@ public class CampResortsByMonth {
 		while(true) {
 			System.out.println("Post Requests Made: " + i);
 			sendPOST();
-			//Thread.sleep(21600000);  //requests every 6 hours
-			//Thread.sleep(7200000);     //requests every 2 hours
-			Thread.sleep(5400000);     //requests every 1.5 hours
+			//Thread.sleep(21600000);    //Requests every 6 hours
+			//Thread.sleep(7200000);     //Requests every 2 hours
+			//Thread.sleep(5400000);     //Requests every 1.5 hours
+			Thread.sleep(300000);        //Requests every 5 minutes
 			i++;
 
 		}
@@ -49,41 +54,35 @@ public class CampResortsByMonth {
 
 			String json = response.toString(); //Holds the request result information in string format
 
+			/*
+			 * The following lines all filter the received JSON to the relevant information. Multiple
+			 * JSONObject variables are needed due to nested JSONs existing.
+			 */
 
-			/* NOTE: The Java built-in JSON package doesn't work for
-			 * this situation, so manual substring-ing was done.
-			 * */
+			Object obj=JSONValue.parse(json);  
+			JSONObject jsonObject = (JSONObject) obj; 
+
+			JSONObject payload = (JSONObject)jsonObject.get("payload");
+			JSONObject available = (JSONObject)payload.get("availability");
+			JSONObject num279 = (JSONObject)available.get("279");
+			JSONObject quote_type_maps = (JSONObject)num279.get("quota_type_maps");
+			JSONObject QuotaUsageByMemberDaily = (JSONObject)quote_type_maps.get("QuotaUsageByMemberDaily");
 
 
-			int ind = json.indexOf(requestedMonth); //Gets information from the first day of the request month
+
+			//Traverses through each day of the month
+			for(int i = 1; i < 32; i++) {
+
+				String date = (i<10) ? (requestedMonth + "-0" + i + "T00:00:00Z") : (requestedMonth + "-" + i + "T00:00:00Z"); //Makes sure date format is correct
+				JSONObject d = (JSONObject) QuotaUsageByMemberDaily.get(date);
 
 
-			while(ind != -1 && ind <= json.length()/2) { //Cut the json length in half because it repeats itself
+				if(d==null) break; //If null, the day does not exist and we are finished looping
 
-				int firstDigitIndex = json.indexOf("remaining", ind); //Gets remaining slots for current day
-				String remaining = json.substring(firstDigitIndex+11, firstDigitIndex+12); //Gets the first digit
+				//If there is a remaining slot, add it to our relevant String
+				if(!d.get("remaining").toString().equals("0")) System.out.println(date.substring(0, 10) + "    Remaining Slots: " + d.get("remaining") + "\n");
 
-				if(Character.isDigit(json.charAt(firstDigitIndex+12))) { //Checks if this is a number greater than 9
-					remaining = remaining + json.charAt(firstDigitIndex+12);
-				}
-
-				if(!remaining.equals("0")) { //If it's not zero, add it as relevant information
-
-					try {Integer.parseInt(remaining);} catch(Exception e){ //Makes sure remaining is actually a number
-
-						ind = json.indexOf(requestedMonth, ind+1);  //Moves to the next day 
-						continue;
-
-					}
-
-					String date = json.substring(ind, ind+10); //Gets entire date
-					System.out.println(date);
-
-				} 
-
-				ind = json.indexOf(requestedMonth, ind+1); //Moves to the next date
-
-			}
+			}	
 
 		} else { //Connection failed
 
@@ -98,6 +97,7 @@ public class CampResortsByMonth {
 		URL u = new URL(reqUrl); //Creates a URL Object to use for connection
 		HttpURLConnection con = (HttpURLConnection) u.openConnection(); //Creates connection with URL object
 		String res = ""; //Holds all relevant information to send to the receiver
+		con.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
 
 
 
@@ -109,118 +109,113 @@ public class CampResortsByMonth {
 			StringBuffer response = new StringBuffer();
 
 
+			//Appends all lines of the JSON into response
 			while ((inputLine = in.readLine()) != null) {
 				response.append(inputLine);
 			}
 			in.close();
 
+
+
 			String json = response.toString(); //Holds the request result information in string format
 
 
-			/* NOTE: The Java built-in JSON package doesn't work for
-			 * this situation, so manual substring-ing was done.
-			 * */
+			/*
+			 * The following lines all filter the received JSON to the relevant information. Multiple
+			 * JSONObject variables are needed due to nested JSONs existing.
+			 */
+
+			Object obj=JSONValue.parse(json);  
+			JSONObject jsonObject = (JSONObject) obj; 
+
+			JSONObject payload = (JSONObject)jsonObject.get("payload");
+			JSONObject available = (JSONObject)payload.get("availability");
+			JSONObject num279 = (JSONObject)available.get("279");
+			JSONObject quote_type_maps = (JSONObject)num279.get("quota_type_maps");
+			JSONObject QuotaUsageByMemberDaily = (JSONObject)quote_type_maps.get("QuotaUsageByMemberDaily");
 
 
-			int ind = json.indexOf(requestedMonth); //Gets information from the first day of the request month
+
+			//Traverses through each day of the month
+			for(int i = 1; i < 32; i++) {
+
+				if(i < 13 || i > 15) continue; //OPTIONAL LINE, DELETE IF YOU WANT ALL DATES OF THE MONTH
+				
+
+				String date = (i<10) ? (requestedMonth + "-0" + i + "T00:00:00Z") : (requestedMonth + "-" + i + "T00:00:00Z"); //Makes sure date format is correct
+				JSONObject d = (JSONObject) QuotaUsageByMemberDaily.get(date);
 
 
-			while(ind != -1 && ind <= json.length()/2) { //Cut the json length in half because it repeats itself
+				if(d==null) break; //If null, the day does not exist and we are finished looping
 
-				int firstDigitIndex = json.indexOf("remaining", ind); //Gets remaining slots for current day
-				String remaining = json.substring(firstDigitIndex+11, firstDigitIndex+12); //Gets the first digit
+				//If there is a remaining slot, add it to our relevant String
+				if(!d.get("remaining").toString().equals("0")) res += date.substring(0, 10) + "    Remaining Slots: " + d.get("remaining") + "\n";
 
-				if(Character.isDigit(json.charAt(firstDigitIndex+12))) { //Checks if this is a number greater than 9
-					remaining = remaining + json.charAt(firstDigitIndex+12);
-				}
+			}	
 
-				if(!remaining.equals("0")) { //If it's not zero, add it as relevant information
 
-					try {Integer.parseInt(remaining);} catch(Exception e){ //Makes sure remaining is actually a number
 
-						ind = json.indexOf(requestedMonth, ind+1);  //Moves to the next day 
-						continue;
 
-					}
+			if(res.length()==0) return; //If not relevant info, don't continue to avoid duplicate information
+			String payloadInfo = "name=Kevin+You&email=kevin.you2940%40gmail.com&message=" + res + "&sub=Send+Message";
 
-					String date = json.substring(ind, ind+10); //Gets entire date
 
-					if(date.substring(8).equals("13") || date.substring(8).equals("14") || date.substring(8).equals("15")) 					
-						res += json.substring(ind, ind+10) + "   " + json.substring(ind+35, ind+46) + remaining + "\n";
 
-				} 
+			//Opens lastReceivedDates.txt file that holds the previously sent information to make sure no
+			//duplicate information is sent again to avoid unnecessary action.
+			File lastReceivedDatesFile = new File("src/lastReceivedDates.txt");
+			FileWriter datesWriter = new FileWriter(lastReceivedDatesFile.getAbsolutePath(), true);
+			Scanner fileReader = new Scanner(lastReceivedDatesFile);
+			String lastDatesStr = ""; //Holds the previous file contents in String format
 
-				ind = json.indexOf(requestedMonth, ind+1); //Moves to the next date
+
+
+			while(fileReader.hasNextLine()) {
+
+				lastDatesStr += fileReader.nextLine() + "\n"; //Appends lastDatesStr
 
 			}
 
-		} else { //Connection failed
 
-			System.out.println("POST REQUEST FAILED. Status Code: " + con.getResponseCode());
+			if (lastDatesStr.equals(res)) { //Checks if the information is a duplicate
 
-		}
+				datesWriter.close();
+				fileReader.close();
+				return;
 
+			} else {
 
+				//Deletes the current files contents and replaces it with the current file contents
+				PrintWriter writer = new PrintWriter(lastReceivedDatesFile.getAbsolutePath());
+				writer.print("");
+				writer.close();		
+				datesWriter.write(res);
 
-		if(res.length()==0) return; //If not relevant info, don't continue to avoid duplicate information
-		String payloadInfo = "name=Kevin+You&email=kevin.you2940%40gmail.com&message=" + res + "&sub=Send+Message";
-
-
-
-		//Opens lastReceivedDates.txt file that holds the previously sent information to make sure no
-		//duplicate information is sent again to avoid unnecessary action.
-		File lastReceivedDatesFile = new File("src/lastReceivedDates.txt");
-		FileWriter datesWriter = new FileWriter(lastReceivedDatesFile.getAbsolutePath(), true);
-		Scanner fileReader = new Scanner(lastReceivedDatesFile);
-		String lastDatesStr = ""; //Holds the previous file contents in String format
-
-
-
-		while(fileReader.hasNextLine()) {
-
-			lastDatesStr += fileReader.nextLine() + "\n"; //Appends lastDatesStr
-
-		}
-
-
-		if (lastDatesStr.equals(res)) { //Checks if the information is a duplicate
+			}
 
 			datesWriter.close();
 			fileReader.close();
-			return;
 
-		} else {
 
-			//Deletes the current files contents and replaces it with the current file contents
-			PrintWriter writer = new PrintWriter(lastReceivedDatesFile.getAbsolutePath());
-			writer.print("");
-			writer.close();		
-			datesWriter.write(res);
+			//Opens a connection to a third-party email sender
+			URL formspree = new URL("https://formspree.io/f/mgebgkrl"); //CHANGE THIS LINK
+			HttpURLConnection form = (HttpURLConnection) formspree.openConnection();
+
+			form.setRequestMethod("POST");
+			form.setDoOutput(true); //Allows OutputStream to be sent out
+			form.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); //IMPORTANT: Information sent should be in URL format
+
+
+
+			//Holds all of the information to send out 
+			OutputStream o = form.getOutputStream();
+			o.write(payloadInfo.getBytes());
+			o.flush();
+			o.close();
+			form.connect(); //Sends the information
+			form.getContentLength();
 
 		}
 
-		datesWriter.close();
-		fileReader.close();
-
-
-		//Opens a connection to a third-party email sender
-		URL formspree = new URL("https://formspree.io/f/mgebgkrl"); //CHANGE THIS LINK
-		HttpURLConnection form = (HttpURLConnection) formspree.openConnection();
-
-		form.setRequestMethod("POST");
-		form.setDoOutput(true); //Allows OutputStream to be sent out
-		form.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); //IMPORTANT: Information sent should be in URL format
-
-
-
-		//Holds all of the information to send out 
-		OutputStream o = form.getOutputStream();
-		o.write(payloadInfo.getBytes());
-		o.flush();
-		o.close();
-		form.connect(); //Sends the information
-		form.getContentLength();
-
 	}
-
 }
